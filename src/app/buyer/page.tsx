342c96ecb1820a1052a750cd95c15478a5a7c1c8
jareershafiq125 <jareershafiq22@gmail.com>
2024-10-11 04:10:36 +0500

    Add files via upload

---
+++ src/app/buyer/page.tsx
@@ -0,0 +1,197 @@
+/* eslint-disable @next/next/no-img-element */
+export default function Buyer() {
+    return(
+      <><section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <a className="block relative h-48 rounded overflow-hidden">
+                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="/img8.png">
+                </img>
+              </a>
+              <div className="mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Modern side table</h2>
+                <p className="mt-1">$16.00</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <a className="block relative h-48 rounded overflow-hidden">
+                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="/img9.png">
+                </img>
+              </a>
+              <div className="mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Double bed</h2>
+                <p className="mt-1">210000pkr</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <a className="block relative h-48 rounded overflow-hidden">
+                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="/img10.png">
+                </img>
+              </a>
+              <div className="mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">stylish bed</h2>
+                <p className="mt-1">120000pkr</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <a className="block relative h-48 rounded overflow-hidden">
+                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="/img11.png">
+                </img>
+              </a>
+              <div className="mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Dressing Table</h2>
+                <p className="mt-1">180000pkr</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <a className="block relative h-48 rounded overflow-hidden">
+                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="/img12.png">
+                </img>
+              </a>
+              <div className="mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Desinger dressing table</h2>
+                <p className="mt-1">16000pkr</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <a className="block relative h-48 rounded overflow-hidden">
+                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="/img13.png">
+                </img>
+              </a>
+              <div className="mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Study Table for kids</h2>
+                <p className="mt-1">21000pkr</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <a className="block relative h-48 rounded overflow-hidden">
+                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="/img14.png">
+                </img>
+              </a>
+              <div className="mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Computer Table</h2>
+                <p className="mt-1">39000pkr</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <a className="block relative h-48 rounded overflow-hidden">
+                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="/img15.png">
+                </img>
+              </a>
+              <div className="mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">The modern sethi</h2>
+                <p className="mt-1">18000pkr</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      
+      /section2
+      <section className="text-gray-600 body-font">
+          <div className="container px-5 py-24 mx-auto">
+            <div className="flex flex-wrap -m-4">
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                <a className="block relative h-48 rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="/img17.png">
+                  </img>
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">Book-shelf</h2>
+                  <p className="mt-1">16000pkr</p>
+                </div>
+              </div>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                <a className="block relative h-48 rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="/img18.png">
+                  </img>
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">Best sofa</h2>
+                  <p className="mt-1">$21.15</p>
+                </div>
+              </div>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                <a className="block relative h-48 rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="/img19.png">
+                  </img>
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">Shoes rack</h2>
+                  <p className="mt-1">120000pkr</p>
+                </div>
+              </div>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                <a className="block relative h-48 rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="/img20.png">
+                  </img>
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">Media wall</h2>
+                  <p className="mt-1">18000pkr</p>
+                </div>
+              </div>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                <a className="block relative h-48 rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="/img21.png">
+                  </img>
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">Kids bed</h2>
+                  <p className="mt-1">1600pkr</p>
+                </div>
+              </div>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                <a className="block relative h-48 rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="/img22.png">
+                  </img>
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">Sethi</h2>
+                  <p className="mt-1">21000pkr</p>
+                </div>
+              </div>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                <a className="block relative h-48 rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="/img23.png">
+                  </img>
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium"> Sofa-Chair</h2>
+                  <p className="mt-1">12000pkr</p>
+                </div>
+              </div>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                <a className="block relative h-48 rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="/img24.png">
+                  </img>
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">Wooden Table</h2>
+                  <p className="mt-1">18000pkr</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section></>
+
+
+    )
+}
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,28 @@
+export default function Profile() {
+  return(
+  <div className="bg-teal-400">
+    <p className= "text-black">Welcome to [ Furiro]
+
+    At Furiro, we believe shopping should be an enjoyable and seamless experience. Our curated collection features the latest trends in fashion, electronics, home goods, and more, all at competitive prices. With a commitment to quality and customer satisfaction, we source products from trusted brands and emerging designers alike.
+    
+    Why Shop With Us?
+    
+    Diverse Selection: Explore a wide range of categories tailored to your needs and preferences.
+    Exceptional Quality: We handpick items to ensure they meet our high standards.
+    User-Friendly Experience: Our website is designed for easy navigation, making your shopping experience simple and efficient.
+    Secure Shopping: Your privacy and security are our top priorities. Shop confidently with our secure payment options.
+    Fast Shipping: Enjoy quick delivery right to your doorstep, so you can start enjoying your purchases sooner.
+    Customer Support: Our dedicated team is here to assist you with any questions or concerns.
+    Join our community of satisfied shoppers today and discover the perfect items to enhance your lifestyle!
+    
+    Feel free to customize it to better fit your brand voice and offerings!
+    
+    
+    
+    
+    
+    </p>
+   
+    </div>
+  )
+}
